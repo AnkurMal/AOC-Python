1d88f49cd2970686c81890b824fca52edc87f837
Ankur Mallick <ankurmallick80@gmail.com>
2025-12-13 09:18:51 +0530

    update

--- problems/year2022/day5.py
+++ problems/year2022/day5.py
@@ -1,19 +1,11 @@
-from collections import deque
-
-
 def part_1():
     with open("data/year2022/day5.txt") as f:
         spl = f.read().split("\n\n")
         spl1 = spl[0].split("\n")
-        vec = []
-
-        for i in range(len(spl1[-1])):
-            if spl1[-1][i].isdigit():
-                queue = deque()
-                for el in spl1[:-1]:
-                    if el[i] != " ":
-                        queue.appendleft(el[i])
-                vec.append(queue)
+        vec = [
+            [el[i] for el in reversed(spl1[:-1]) if el[i] != " "]
+            for i in range(1, len(spl1[-1]), 4)
+        ]
 
         for i in spl[1].split("\n"):
             ins = [int(j) - 1 for j in i.split()[1::2]]
@@ -30,25 +22,20 @@ def part_2():
     with open("data/year2022/day5.txt") as f:
         spl = f.read().split("\n\n")
         spl1 = spl[0].split("\n")
-        vec = []
-
-        for i in range(len(spl1[-1])):
-            if spl1[-1][i].isdigit():
-                queue = deque()
-                for el in spl1[:-1]:
-                    if el[i] != " ":
-                        queue.appendleft(el[i])
-                vec.append(queue)
+        vec = [
+            [el[i] for el in reversed(spl1[:-1]) if el[i] != " "]
+            for i in range(1, len(spl1[-1]), 4)
+        ]
 
         for i in spl[1].split("\n"):
             ins = [int(j) - 1 for j in i.split()[1::2]]
-            local = deque()
+            local = []
 
             for j in range(ins[0] + 1):
                 if len(vec[ins[1]]) == 0:
                     break
-                local.appendleft(vec[ins[1]].pop())
-            vec[ins[2]].extend(local)
+                local.append(vec[ins[1]].pop())
+            vec[ins[2]].extend(reversed(local))
 
         st = "".join(s.pop() for s in vec)
         print("Part 2:", st)
