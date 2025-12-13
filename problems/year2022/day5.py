61ebbbb84b7dfa3bab28ddeea113c9bea3df18e3
Ankur Mallick <ankurmallick80@gmail.com>
2025-12-13 09:32:21 +0530

    update

--- problems/year2022/day5.py
+++ problems/year2022/day5.py
@@ -1,41 +1,32 @@
+with open("data/year2022/day5.txt") as f:
+    spl = f.read().split("\n\n")
+spl1 = spl[0].split("\n")
+vec = [
+    [el[i] for el in reversed(spl1[:-1]) if el[i] != " "]
+    for i in range(1, len(spl1[-1]), 4)
+]
+
+
 def part_1():
-    with open("data/year2022/day5.txt") as f:
-        spl = f.read().split("\n\n")
-        spl1 = spl[0].split("\n")
-        vec = [
-            [el[i] for el in reversed(spl1[:-1]) if el[i] != " "]
-            for i in range(1, len(spl1[-1]), 4)
-        ]
+    for i in spl[1].split("\n"):
+        ins = [int(j) - 1 for j in i.split()[1::2]]
+        for j in range(ins[0] + 1):
+            if len(vec[ins[1]]) == 0:
+                break
+            vec[ins[2]].append(vec[ins[1]].pop())
 
-        for i in spl[1].split("\n"):
-            ins = [int(j) - 1 for j in i.split()[1::2]]
-            for j in range(ins[0] + 1):
-                if len(vec[ins[1]]) == 0:
-                    break
-                vec[ins[2]].append(vec[ins[1]].pop())
-
-        st = "".join(s.pop() for s in vec)
-        print("Part 1:", st)
+    print("Part 1:", "".join(s.pop() for s in vec))
 
 
 def part_2():
-    with open("data/year2022/day5.txt") as f:
-        spl = f.read().split("\n\n")
-        spl1 = spl[0].split("\n")
-        vec = [
-            [el[i] for el in reversed(spl1[:-1]) if el[i] != " "]
-            for i in range(1, len(spl1[-1]), 4)
-        ]
+    for i in spl[1].split("\n"):
+        ins = [int(j) - 1 for j in i.split()[1::2]]
+        local = []
 
-        for i in spl[1].split("\n"):
-            ins = [int(j) - 1 for j in i.split()[1::2]]
-            local = []
+        for j in range(ins[0] + 1):
+            if len(vec[ins[1]]) == 0:
+                break
+            local.append(vec[ins[1]].pop())
+        vec[ins[2]].extend(reversed(local))
 
-            for j in range(ins[0] + 1):
-                if len(vec[ins[1]]) == 0:
-                    break
-                local.append(vec[ins[1]].pop())
-            vec[ins[2]].extend(reversed(local))
-
-        st = "".join(s.pop() for s in vec)
-        print("Part 2:", st)
+    print("Part 2:", "".join(s.pop() for s in vec))
